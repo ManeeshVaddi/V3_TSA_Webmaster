fb2d0d1030d5f96beced6b995e92175f641cf3f3
Maneesh Vaddi <68968976+ManeeshVaddi@users.noreply.github.com>
2024-05-20 18:32:53 -0400

    Add files via upload

---
+++ V3_TSA_Webmaster/index.js
@@ -0,0 +1,11 @@
+function player(){
+    
+}
+
+function myFunction() {
+    if (document.documentElement.scrollTop > 50) {
+      document.getElementById("myP").className = "test";
+    } else {
+      document.getElementById("myP").className = "";
+    }
+  }
