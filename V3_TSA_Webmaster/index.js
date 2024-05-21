3dd7ee6e53d290b09e6975546e3e3dc9d6092710
Maneesh Vaddi <Maneeshvaddi@gmail.com>
2024-05-21 18:42:56 -0400

    Worked w team and did hella stuff
    crazy day did so much work WWW

--- V3_TSA_Webmaster/index.js
+++ V3_TSA_Webmaster/index.js
@@ -1,11 +1,43 @@
-function player(){
-    
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+
+menuBtn.onclick = () =>{
+   menu.classList.add("active");
+   menuBtn.classList.add("hide");
+   cancelBtn.classList.remove("hide");
+}
+cancelBtn.onclick = () =>{
+   menu.classList.remove("active");
+   cancelBtn.classList.add("hide");
+   menuBtn.classList.remove("hide");
 }
 
-function myFunction() {
-    if (document.documentElement.scrollTop > 50) {
-      document.getElementById("myP").className = "test";
-    } else {
-      document.getElementById("myP").className = "";
-    }
-  }
+
+window.onscroll = ()=>{
+   this.scrollY > 50 ? navbar.classList.add("sticky"): navbar.classList.remove("sticky");
+}
+
+window.addEventListener('scroll', function() {
+   const navbar = document.getElementById('navbar');
+   const sections = document.querySelectorAll('div[id]');
+   
+   sections.forEach(section => {
+       const sectionTop = section.offsetTop - navbar.offsetHeight;
+       const sectionBottom = sectionTop + section.offsetHeight;
+
+       if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+           const sectionId = section.getAttribute('id');
+           const activeLink = document.querySelector(`.navbar a[href="#${sectionId}"]`);
+           
+           document.querySelectorAll('.navbar a').forEach(link => {
+               link.classList.remove('active', 'underline');
+           });
+
+           activeLink.classList.add('active', 'underline');
+       }
+   });
+});
+
